d4e310b41071449bf95ec3e7a52236c9888c9094
Annet Michell Medina Pablo <michellmepa06@gmail.com>
2025-07-07 18:11:56 -0600

    carrito custome hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header.jsx';
 import useLocalStorage from './hooks/useLocalStorage.js';
 
 function App() {
-  const [carrito, setCarrito] = useLocalStorage([]);
+  const [carrito, setCarrito] = useLocalStorage("carrito", []);
   const [busqueda, setBusqueda] = useState(''); 
 
   return (
